7c6dbb9e679a335333481b02a60f52bd5bf5cc5a
Anand A J <anandjayakumar2001@gmail.com>
2026-01-25 20:08:27 +0530

    Add auth controller

---
+++ src/InvoicePro.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+using InvoicePro.API.Core;
+using InvoicePro.Application.DTOs.Auth;
+using InvoicePro.Application.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicePro.API.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequestDto req)
+    {
+        var result = await _authService.RegisterAsync(req);
+        return Ok(ApiResponse<AuthResponseDto>.Ok(result, "registered successfully"));
+    }
+}
